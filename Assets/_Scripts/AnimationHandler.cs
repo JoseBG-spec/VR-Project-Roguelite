c1a370031a1d5ddc2975b8891267f34e638eef35
Alex Helo <itsalexhelo@gmail.com>
2022-11-05 19:51:44 -0600

    Finished basic enemy animations

--- Assets/_Scripts/AnimationHandler.cs
+++ Assets/_Scripts/AnimationHandler.cs
@@ -4,15 +4,50 @@ using UnityEngine;
 
 public class AnimationHandler : MonoBehaviour
 {
+
+    public float range;
+    public float hitRange;
+    public Transform player;
+    public Animator animator; 
+    public float yOffset;
     // Start is called before the first frame update
     void Start()
     {
+        animator = GetComponent<Animator>();
         
+
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
+
+        if (Vector3.Distance(player.position, new Vector3 (transform.position.x, transform.position.y + yOffset, transform.position.z)) <= range && Vector3.Distance(player.position, new Vector3 (transform.position.x, transform.position.y + yOffset, transform.position.z)) >= hitRange){
+             animator.SetBool("Spotted", true);
+        
+
+         } else if (Vector3.Distance(player.position, new Vector3 (transform.position.x, transform.position.y + yOffset, transform.position.z)) <= hitRange){
+            animator.SetBool("Spotted", false);
+  
+              if (Random.value >= 0.5)
+                {
+                     animator.SetBool("HitRangeL", true);
+                     animator.SetBool("HitRangeR", false);
+                }
+                    animator.SetBool("HitRangeL", false);
+                    animator.SetBool("HitRangeR", true);
+                }
         
     }
+
+      void OnDrawGizmos()
+    {   
+        // Draw a yellow sphere at the transform's position
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(new Vector3 (transform.position.x, transform.position.y + yOffset, transform.position.z), range);
+
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireSphere(new Vector3 (transform.position.x, transform.position.y + yOffset, transform.position.z), hitRange);
+    }
+
 }
