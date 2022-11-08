243cc187dc161c97c4c65dbd9c30d95a6c152288
José Antonio Berrón Gutiérrez <61696362+JoseBG-spec@users.noreply.github.com>
2022-11-07 21:35:54 -0600

    Added Slicing to the main game

--- Assets/_Scripts/SwordCut.cs
+++ Assets/_Scripts/SwordCut.cs
@@ -7,22 +7,26 @@ public class SwordCut : MonoBehaviour
 {
     [SerializeField] private Material crossMaterial;
     [SerializeField] private Transform cutPlane;
-    public void Slice(GameObject sliceObject)
+    public void Slice(GameObject sliceObject, GameObject parent)
     {
         SlicedHull hull = SliceObject(sliceObject, crossMaterial);
 
         GameObject bottom = hull.CreateLowerHull(sliceObject, crossMaterial);
         GameObject top = hull.CreateUpperHull(sliceObject, crossMaterial);
-        AddHullComponents(bottom);
-        AddHullComponents(top);
+        AddHullComponents(bottom, parent);
+        AddHullComponents(top, parent);
         Destroy(sliceObject);
+        Destroy(parent);
     }
 
-    public void AddHullComponents(GameObject go)
+    public void AddHullComponents(GameObject go,GameObject parent)
     {
+        go.transform.localPosition = parent.transform.localPosition;
+        go.transform.localRotation = parent.transform.localRotation;
+        //go.transform.localScale = go.transform.localScale;
         //go.layer = 6;
-        //Rigidbody rb = go.AddComponent<Rigidbody>();
-        //rb.interpolation = RigidbodyInterpolation.Interpolate;
+        Rigidbody rb = go.AddComponent<Rigidbody>();
+        rb.interpolation = RigidbodyInterpolation.Interpolate;
         MeshCollider collider = go.AddComponent<MeshCollider>();
         collider.convex = true;
 
@@ -46,17 +50,29 @@ public class SwordCut : MonoBehaviour
         }   
         if (collisionObject.layer == 6)
         {
+            Mesh bakedMesh = new Mesh();
             GameObject firstActiveGameObject = null;
+            Material meshMaterial = null;
             for (int i = 0; i < collisionObject.transform.childCount; i++)
             {
                 if (collisionObject.transform.GetChild(i).gameObject.activeSelf)
                 {
                     firstActiveGameObject = collisionObject.transform.GetChild(i).gameObject;
                     Debug.Log(firstActiveGameObject.name);
+
+                    firstActiveGameObject.GetComponent<SkinnedMeshRenderer>().BakeMesh(bakedMesh);
+                    meshMaterial = firstActiveGameObject.GetComponent<SkinnedMeshRenderer>().material;
+
+                    firstActiveGameObject.AddComponent<MeshFilter>();
+                    firstActiveGameObject.AddComponent<MeshRenderer>();
+                    firstActiveGameObject.GetComponent<MeshFilter>().mesh = bakedMesh;
+                    firstActiveGameObject.GetComponent<MeshRenderer>().material = meshMaterial;
+
+                    Destroy(firstActiveGameObject.GetComponent<SkinnedMeshRenderer>());
                     break;
                 }
             }
-            Slice(firstActiveGameObject);
+            Slice(firstActiveGameObject,collisionObject);
             collisonOccured = true;
         }
     }
